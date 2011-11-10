6bdc506392740231f515d7f4e864a45f6396ecbc
Stephan L <stephan89@gmx.net>
2011-11-10 23:57:56 +0100

    User koennen nun erstellt werden und sich einloggen. fehlermeldungen werden gesendet

---
+++ application/controllers/MapController.php
@@ -0,0 +1,20 @@
+<?php
+
+class MapController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+       
+    }
+
+   
+	public function indexAction()
+	{
+		$this->_helper->viewRenderer->setNoRender(true);
+		echo '<a href="/ajax/logout">logout</a>';
+	}
+	
+	
+	
+}
