3e6f93e92bac285817064bedd6fae610e7e726a8
Timo Ge <nicht.meine.email.adresse@web.de>
2011-11-26 15:06:05 +0100

    merke commit/nichts neues

--- application/controllers/MapController.php
+++ application/controllers/MapController.php
@@ -14,12 +14,29 @@ class MapController extends Zend_Controller_Action
 		
 	}
 	
-	public function bilderAction(){
-		echo 'hier steht php code';
+	public function getJsonAction(){
+		$this->_helper->layout()->disableLayout();
+		$user = Application_Model_AuthUser::getAuthUser();
+		$this->view->arrLogs = $user->getLogs();
+		/*
+		$arrLogs = $user->getLogs();
+		$arrJsonObjs = array();
+		foreach ($arrLogs as $objLog){
+			array_push($arrJsonObjs, 
+				array( 	'latitude' => $objLog->getLat(),
+						'longitude' => $objLog->getLong(),
+						'image' => 'daten/pics/orig/' . $objLog->getPicIdent() . '.jpg'
+				)
+			);
+		}
+		$jsonData = Zend_Json::encode($arrJsonObjs);
+		echo $jsonData;
+		*/
 	}
 	
 	public function getImagesForTimelineAction(){
 		$this->_helper->layout()->disableLayout();		
 		$this->view->anzahlBilder = $this->getRequest()->getParam("number-of-images");
 	}
+
 }
