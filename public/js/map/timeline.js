98c78e51c9384bc54f4aa699cd894c7dc9909c8a
Timo Ge <nicht.meine.email.adresse@web.de>
2011-12-02 00:51:46 +0100

    timline änderung
    http://localhost/map/get-images-for-timeline/number-of-images/16/first-d
    ate/1-12-2011/second-date/2-12-2011

--- public/js/map/timeline.js
+++ public/js/map/timeline.js
@@ -20,6 +20,8 @@ $(window).resize(function() {
 });
 
 $(document).ready(function() {	
+	
+	
  						
 	var dates = $( "#zeitraumStartEingabefeldId, #zeitraumEndeEingabefeldId" ).datepicker({
 		showOn: "button",	
@@ -84,6 +86,13 @@ $(document).ready(function() {
 		parseDate();
 		sendImageRequest();
 	});
+	
+	$('#addFriendsTextfieldId').keyup(function(element){
+		$('#friendSearchList').empty();	
+		value1 = $('#addFriendsTextfieldId').val();
+		console.log($('#addFriendsTextfieldId').val());
+		$('#friendSearchList').load('map/get-user-for-timeline/var1/' + value1);
+	});
 });
 
 function howMuchImages(){
@@ -134,11 +143,12 @@ function sendImageRequest(){
 	
 	setDateDifference();
 	howMuchImages();
-	
 	//hourDifference = Math.round((secondDate - firstDate) / (1000*60*60));
-	console.log(firstDate.toShortTimeString());
-	//console.log(hourDifference)
-	
-	$('#images').load('map/get-images-for-timeline/number-of-images/' + numberOfImages + "/first-day/" + firstDay + "/first-month/" + firstMonth + "/first-year/" + firstYear + "/second-day/" + secondDay + "/second-month/" + secondMonth + "/second-year/" + secondYear);
+
+	$('#images').load('map/get-images-for-timeline/number-of-images/' + numberOfImages + "/first-date/" + firstDate.toString('d-M-yyyy') + "/second-date/" + secondDate.toString('d-M-yyyy'));
 	//$('#images').load('map/get-images-for-timeline/' + 'id/' + 'minus/' + '/start/' + $(zeitraumStartEingabefeldId).val() + '/ende/' +$(zeitraumEndeEingabefeldId).val());
 }
+
+
+
+	
